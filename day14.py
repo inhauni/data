b0c81fe455f193bd283631c1e00e741071ed6c15
inhauni <inhauni123@gmail.com>
2023-02-02 15:05:28 +0900

    pokemon game v0.2

--- day14.py
+++ day14.py
@@ -1,23 +1,82 @@
-
+# pokemon game v0.2
+# 중복코드 제거, getter setter
 
 class Pokemon:
-
-    def __init__(self, name, owner, skills): # name이라는 매개변수
-        self.name = name # 받은 name을 속성.name에 저장
-        # print(f'포켓몬 {name} 객체 생성됨')
-        self.owner = owner # 받은 owner변수를 속성.owner에 저장
+    def __init__(self, owner, skills):
+        self.hidden_owner = owner
         self.skills = skills.split('/')
+        print(f"포켓몬 생성 :", end=' ')
+
+    def get_owner(self):
+        return self.hidden_owner
+
+    def set_owner(self, owner):
+        self.hidden_owner = owner
+
     def info(self):
-        print(f'{self.owner}의 포켓몬은 {self.name}입니다.')
-        for skill in self.skills:
-            print(skill,end=' ')
+        print(f"{self.get_owner()}의 포켓몬이 사용 가능한 스킬")
+        for i in range(len(self.skills)):
+            print(f'{i+1} : {self.skills[i]}')
 
-#<상속>
-class Pikachu(Pokemon): # 상속(inheritance) : 부모 class 는 pokemon / 자식 class는 pickchu
-    pass
+        # for skill in self.skills:
+        #     print(f'{skill}')
 
-p1=Pokemon('피카츄','지우','백만 볼트/몸통박치기/전광속화') # 객체 생성
-p2=Pokemon('꼬부기','이슬이','물대포/몸통박치기')
+    def attack(self, idx):
+        print(f'{self.skills[idx]} 공격 시전!')
 
-pi1 =Pikachu('피카츄','덴트','번개')
-pi1.info()
+
+class Pikachu(Pokemon):  # inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "피카츄"
+        print(f"{self.name}")
+
+    def attack(self, idx):  # override
+        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
+
+
+class Ggoboogi(Pokemon):  # inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "꼬부기"
+        print(f"{self.name}")
+
+    def attack(self, idx):  # override
+        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
+
+    def swim(self):
+        print(f'{self.name}가 수영을 합니다')
+
+
+class Pairi(Pokemon):  # inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "파이리"
+        print(f"{self.name}")
+
+    def attack(self, idx):  # override
+        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
+
+
+while True:
+    menu = input('1) 포켓몬 생성  2) 프로그램 종료 : ')
+    if menu == '2':
+        print('프로그램을 종료합니다')
+        break
+    elif menu == '1':
+        pokemon = input('1) 피카츄  2) 꼬부기  3) 파이리 : ')
+        n = input('플레이어 이름 입력 : ')
+        s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
+        if pokemon == '1':
+            p = Pikachu(n, s)
+        elif pokemon == '2':
+            p = Ggoboogi(n, s)
+        elif pokemon == '3':
+            p = Pairi(n, s)
+        else:
+            print('메뉴에서 골라 주세요')
+        p.info()
+        attack_menu = input('공격 번호 선택 : ')
+        p.attack(int(attack_menu)-1)
+    else:
+        print('메뉴에서 골라 주세요')
