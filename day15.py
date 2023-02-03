77caa09830c6a0fe84f8f914fea6cd3e12b46776
inhauni <inhauni123@gmail.com>
2023-02-03 14:00:43 +0900

    poly2

--- day15.py
+++ day15.py
@@ -98,43 +98,50 @@
 
     ## 함수 선언 부분 ##
 
-def printPoly(t_x, p_x):
+def printPoly(p_x):
+
+    term = len(p_x)-1  # 최고차항 차수
+
     polyStr = "P(x) = "
 
     for i in range(len(p_x)):
-        term = t_x[i]  # 항 차수
+
         coef = p_x[i]  # 계수
 
-        if (coef >= 0):
+        if coef == 0:
+            term = term-1
+            continue
+        if coef>=0:
             polyStr += "+"
-            polyStr += str(coef) + "x^" + str(term) + " "
 
+        polyStr += f'{coef}x^{term} '
+        term -= 1
     return polyStr
 
 
-def calcPoly(xVal, t_x, p_x):
+def calcPoly(xVal, p_x):
     retValue = 0
-
+    term = len(p_x) - 1 # 차수
     for i in range(len(p_x)):
-        term = t_x[i]  # 항 차수
         coef = p_x[i]  # 계수
         retValue += coef * xValue ** term
+        term -= 1
 
     return retValue
 
 
     ## 전역 변수 선언 부분 ##
 
-tx = [300, 20, 0]
-px = [7, -4, 5]
+# tx = [300, 20, 0]
+px = [7, -4, 0, 5]
 
     ## 메인 코드 부분 ##
 if __name__ == "__main__":
-    pStr = printPoly(tx, px)
+    pStr = printPoly(px)
     print(pStr)
 
     xValue = int(input("X 값-->"))
 
-    pxValue = calcPoly(xValue, tx, px)
+    pxValue = calcPoly(xValue, px)
     print(pxValue)
 
