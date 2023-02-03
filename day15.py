39db2ae618ecc8d5091a6fe755b72c156da08a35
inhauni <inhauni123@gmail.com>
2023-02-03 13:29:06 +0900

    pokemon add insert delete

--- day15.py
+++ day15.py
@@ -3,61 +3,50 @@
 
 
 ## <데이터 추가 코드>
-#
-# def add_data(pokemon):
-#     Pokemons.append(None)
-#     # Pokemons[len(Pokemons) - 1] = pokemon
-#     Pokemons[- 1] = pokemon  # 맨끝에 집어넣는 방법
-#
-#
-# add_data('피카추')
-# add_data('라이츄')
-# add_data('파이리')
-# add_data('꼬부기')
-# add_data('버터풀')
-#
-# print(Pokemons)
-#
+
+def add_data(pokemon):
+    pokemons.append(None)
+    # Pokemons[len(Pokemons) - 1] = pokemon
+    pokemons[- 1] = pokemon  # 맨끝에 집어넣는 방법
 
 
-Pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀"]
-
-
+# Pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀"]
 
 # <데이터 삽입 코드>
-def insert_data(position, pokemon):
-    if position < 0 or position > len(Pokemons):
+def insert_data (position, pokemon):
+
+    if position < 0 or position > len(pokemons):
         print("Out of range.")
         return
 
-    Pokemons.append(None)  # 빈칸 추가
-    len_pokemon = len(Pokemons)  # 배열의 현재 크기
+    pokemons.append(None)  # 빈칸 추가
+    len_pokemon = len(pokemons)  # 배열의 현재 크기
 
     for i in range(len_pokemon - 1, position, -1):  # 맨끝에 데이터 삽입시 이 코드 안 돌아감
-        Pokemons[i] = Pokemons[i - 1]
-        Pokemons[i - 1] = None
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
 
-    Pokemons[position] = pokemon  # 지정한 위치에 친구 추가
+    pokemons[position] = pokemon  # 지정한 위치에 친구 추가
 
 # <데이터 삭제 코드>
 def delete_data(idx):
 
-    if idx < 0 or idx > len(Pokemons)-1:
+    if idx < 0 or idx > len(pokemons):
         print("Out of range")
         return
 
-    len_pokemons = len(Pokemons)
-    Pokemons[idx] = None  # 데이터 삭제
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
 
     for i in range(idx + 1, len_pokemons):
-        Pokemons[i - 1] = Pokemons[i]
-        Pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
 
-    del (Pokemons[len_pokemons - 1])
+    del (pokemons[len_pokemons - 1])
 
-# < idx 뒤의 데이터 전부 삭제>
-# def delete_data(idx):
-#
+#< idx 뒤의 데이터 전부 삭제>
+
+#def delete_data(idx):
 #     len_pokemons = len(Pokemons)
 #
 #     if idx < 0 or idx > len(Pokemons)-1:
@@ -68,13 +57,43 @@ def delete_data(idx):
 #         del (Pokemons[i])
 
  # 메인으로 지정
-if __name__ == "__main__":
 
-    insert_data(4, '어니부기')
-    print(Pokemons)
-    insert_data(5, '거북왕')
-    print(Pokemons)
-    delete_data(7)
-    print(Pokemons)
-    delete_data(4)
-    print(Pokemons)
+
+
+pokemons = []
+select = -1    #
+
+
+if __name__ == "__main__":
+    # insert_data(4, '어니부기')
+    # print(Pokemons)
+    # insert_data(5, '거북왕')
+    # print(Pokemons)
+    # delete_data(7)
+    # print(Pokemons)
+    # delete_data(4)
+    # print(Pokemons)
+
+    while select != 4:
+
+        select = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
+
+        if (select == 1):
+            data = input("추가할 데이터--> ")
+            add_data(data)
+            print(pokemons)
+        elif (select == 2):
+            idx = int(input("삽입할 위치--> "))
+            data = input("추가할 데이터--> ")
+            insert_data(idx, data)
+            print(pokemons)
+        elif (select == 3):
+            idx = int(input("삭제할 위치--> "))
+            delete_data(idx)
+            print(pokemons)
+        elif (select == 4):
+            print(pokemons)
+            exit
+        else:
+            print("1~4 중 하나를 입력하세요.")
+            continue
