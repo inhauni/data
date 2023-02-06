69f85d793c339c5665e6e926fb4ad9df4108d557
inhauni <inhauni123@gmail.com>
2023-02-06 23:41:40 +0900

    5-1

--- day16practice.py
+++ day16practice.py
@@ -1,22 +1,85 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
+# 5장 01
+import random
+import math
+
+class Node():
+    def __init__(self):
+        self.data=None
         self.link=None
 
-n1 = Node('Pikachu')
-n1.link=n1
-head=n1
 
-n2= Node('Raichu')
-n1.link=n2
-n2.link=head
+def printNode(start):
+    current=start
+    if current==None:
+        return
+    print(f'{current.data[0]} 편의점 , 거리 : {current.data[1]}', end='\n')
+    while current.link != head:
+        current=current.link
+        print(f'{current.data[0]} 편의점 , 거리 : {current.data[1]}', end=' \n')
+    print()
 
-n3=Node('pyrii')
-n2.link=n3
-n3.link=head
+def arrayConv(store):
 
-current=head
-print(current.data, end=' ')
-while current.link!=head:
-    current=current.link
-    print(current.data,end=' ')
+    global memory, current, head, pre
+
+    node=Node()
+    node.data=store
+
+    memory.append(node)
+
+    if head == None:
+        head=node
+        node.link=head
+
+        return
+
+
+
+    if head.data[1]>store[1]:
+        node.link=head
+        last=head
+        while last.link!=head:
+            last=last.link
+        last.link=node
+        head=node
+
+        return
+
+    current=head
+    while current.link != head:
+        pre=current
+        current=current.link
+        if current.data[1] > store[1]:
+            pre.link = node
+            node.link = current
+            return
+
+    current.link = node
+    node.link=head
+
+
+
+
+memory=[]
+head, pre, current = None, None, None
+
+alph=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+if __name__ == "__main__":
+
+    convs=[]
+    for i in alph:               # 편의점 객체들 생성
+        convName = i
+        conx=random.randint(1,100)
+        cony=random.randint(1,100)
+        conDist=math.sqrt(conx*conx+cony*cony)
+        conv = convName,conDist
+        convs.append(conv)
+
+
+    for store in convs:
+        arrayConv(store)
+
+    printNode(head)
+
+
