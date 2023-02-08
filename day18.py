cb73646f1b49dfd04201bbc3c48cd4a6fd8f6527
inhauni <inhauni123@gmail.com>
2023-02-08 13:37:43 +0900

    graph 3 - queue

--- day18.py
+++ day18.py
@@ -1,37 +1,52 @@
-## 함수 선언 부분 ##
+## 클래스 및 함수 선언 부분 ##
 class Graph() :
 	def __init__ (self, size) :
 		self.SIZE = size
 		self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
-def printGraph(g) :
-	print(' ', end = ' ')
-	for v in range(g.SIZE) :
-		print(nameAry[v], end = ' ')
-	print()
-	for row in range(g.SIZE) :
-		print(nameAry[row], end = ' ')
-		for col in range(g.SIZE) :
-			print(g.graph[row][col], end= '  ')
-		print()
-	print()
-
-
 ## 전역 변수 선언 부분 ##
 G1 = None
-nameAry = ['문별', '솔라', '휘인', '쯔위', '선미', '화사']
-문별, 솔라, 휘인, 쯔위, 선미, 화사 = 0, 1, 2, 3, 4, 5
-
+queue = []
+visitedAry = []		# 방문한 정점
 
 ## 메인 코드 부분 ##
-gSize = 6
-G1 = Graph(gSize)
-G1.graph[문별][솔라] = 1; G1.graph[문별][휘인] = 1
-G1.graph[솔라][문별] = 1; G1.graph[솔라][쯔위] = 1
-G1.graph[휘인][문별] = 1; G1.graph[휘인][쯔위] = 1
-G1.graph[쯔위][솔라] = 1; G1.graph[쯔위][휘인] = 1; G1.graph[쯔위][선미] = 1; G1.graph[쯔위][화사] = 1
-G1.graph[선미][쯔위] = 1; G1.graph[선미][화사] = 1
-G1.graph[화사][쯔위] = 1; G1.graph[화사][선미] = 1
+G1 = Graph(4)
+G1.graph[0][2] = 1; G1.graph[0][1] = 1;
+G1.graph[1][2] = 1;G1.graph[1][0] = 1;G1.graph[1][3] = 1;
+G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1;
+G1.graph[3][1] = 1; G1.graph[3][2] = 1
+
 
 print('## G1 무방향 그래프 ##')
-printGraph(G1)
+for row in range(4) :
+	for col in range(4) :
+		print(G1.graph[row][col], end = ' ')
+	print()
+
+current = 0		# 시작 정점 A
+queue.append(current)
+visitedAry.append(current)
+
+while (len(queue) != 0) :
+	next = None
+	for vertex in range(4) :
+		if G1.graph[current][vertex] == 1 :
+			if vertex in visitedAry :  	   # 방문한 적이 있는 정점이면 탈락
+				pass
+			else : 			   # 방문한 적이 없으면 다음 정점으로 지정
+				next = vertex
+				break
+
+	if next is not None :			  	   # 다음에 방문할 정점이 있는 경우
+		current = next
+		queue.append(current)
+		visitedAry.append(current)
+	else :            	  	  	  	  # 다음에 방문할 정점이 없는 경우
+		current = queue.pop()  # Queue일 경우 오버헤드 발생 ->
+                                # 앞에걸 뺀 후 뒤의 공백을 만들기 위해 자료들을 한칸씩 앞으로 당기는 작업이 필요
+                                # if 자료의 수가 많으면 오버헤드 발생
+
+
+print('방문 순서 -->', end='')
+for i in visitedAry :
+	print(chr(ord('A')+i), end='   ')
