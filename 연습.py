8708fa141a3610ed45a0d53cf844008a7084059b
inhauni <inhauni123@gmail.com>
2023-02-08 18:20:01 +0900

    graph ãMinimum Cost Spanning Tree practice

--- 연습.py
+++ 연습.py
@@ -21,54 +21,111 @@ class Graph:
                     print(self.graph[row][col], end='   ')
                 print()
 
+
+
+##  누락요소 확인 (DFS와 유사)
+
+def findVertex(g,findV):
+
+    stack=[]
+    visited=[]
+
+    current=0
+    stack.append(0)
+    visited.append(0)
+
+
+    while len(stack) != 0:
+
+        next=None
+        for vertex in range(g.size):
+            if g.graph[current][vertex]!=0:
+                    if vertex in visited:
+                        pass
+                    else:
+                        next = vertex
+                        break
+
+        if next is not None:
+            current=next
+            stack.append(current)
+            visited.append(current)
+
+        else:
+            current=stack.pop()
+
+    if findV in visited:
+        return True
+    return False
+
 ## 전역변수 설정
 
-G1=None
-NameAry=['화사', '문별', '휘인', '솔라']
-stack=[]
-visited=[]
+g=None
+NameAry=['춘천', '서울', '속초', '대전', '광주', '부산']
+춘천,서울,속초,대전,광주,부산=0,1,2,3,4,5  # 도시를 전역변수로 설정
 
 ## 그래프 생성
 
-g1=Graph(4)
-g1.graph[0][3]=1;
-g1.graph[1][2]=1;g1.graph[1][3]=1;
-g1.graph[2][1]=1;
-g1.graph[3][0]=1;g1.graph[3][1]=1;
+g=Graph(6)
+g.graph[춘천][서울] = 10; g.graph[춘천][속초] = 15
+g.graph[서울][춘천] = 10; g.graph[서울][속초] = 40; g.graph[서울][대전] = 11; g.graph[서울][광주] = 50
+g.graph[속초][춘천] = 15; g.graph[속초][서울] = 40; g.graph[속초][대전] = 12
+g.graph[대전][서울] = 11; g.graph[대전][속초] = 12; g.graph[대전][광주] = 20; g.graph[대전][부산] = 30
+g.graph[광주][서울] = 50; g.graph[광주][대전] = 20; g.graph[광주][부산] = 25
+g.graph[부산][대전] = 30; g.graph[부산][광주] = 25
 
 print('----생성된 그래프----\n')
-g1.printGraph(NameAry)
+g.printGraph(NameAry)
+print()
 
 
-# 깊이 우선 탐색 구현 (DFS)
 
-current=0
-root=current
-stack.append(0)
-visited.append(0)
+# 가중치 간선 목록
 
 
-while len(stack) != 0:
 
-    next=None
-    for vertex in range(g1.size):
-        if g1.graph[current][vertex]==1:
-                if vertex in visited:
-                    pass
-                else:
-                    next = vertex
-                    break
+## 중복 없는 가중치 & 간선 목록 생성
+edges=[]
 
-    if next is not None:
-        current=next
-        stack.append(current)
-        visited.append(current)
+for i in range(g.size):
+    for k in range(g.size):
+        if g.graph[i][k]!=0:
+            edges.append([g.graph[i][k],i,k])
 
+from operator import itemgetter
+
+edges=sorted(edges,key=itemgetter(0),reverse=True)
+
+newedges=[]
+for i in range(0,len(edges),2):
+    newedges.append(edges[i])
+
+
+## 가중치가 높은 간선부터 제거
+
+index=0
+
+while len(newedges) >g.size-1:
+    start=newedges[index][1]
+    end=newedges[index][2]
+    save=newedges[index][0]
+
+    g.graph[start][end]=0
+    g.graph[end][start]=0
+
+    startYN=findVertex(g,start)
+    endYN=findVertex(g,end)
+
+    if startYN and endYN:
+        del(newedges[index])
     else:
-        current=stack.pop()
+
+        g.graph[start][end] = save
+        g.graph[end][start] = save
+
+        index += 1
 
 
-print(f'\n방문순서 : ',end=' ')
-for visit in visited:
-    visit=NameAry[visit]
-    print(visit,end=' ')
+print('----최소비용으로 생성된 그래프----\n')
+g.printGraph(NameAry)
+print()
